484a0f8ce4583751da8dc7388424adcb2953ebd5
Daniel Woodworth <danwoodworth.dw@gmail.com>
2024-08-16 16:08:55 -0500

    Header and hero image updates

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,14 +1,13 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Grant Dental",
+    "email": "grantDental@gmail.com",
+    "phoneForTel": "402-779-4407",
+    "phoneFormatted": "(402) 779-4407",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "1001 Grant Plaza",
+        "city": "Omaha",
+        "state": "NE",
+        "zip": "68046",
         "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
     "socials": {
